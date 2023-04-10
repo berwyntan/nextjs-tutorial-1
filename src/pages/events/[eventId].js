aa1630fd7508c14cf1162297bcd7dd5ef5e7a7ea
Berwyn Tan <berwyntan@gmail.com>
2023-04-10 20:22:34 +0800

    add getStaticProps

--- src/pages/events/[eventId].js
+++ src/pages/events/[eventId].js
@@ -1,30 +1,86 @@
-import { useRouter } from "next/router"
-import { getEventById } from "../../../dummy-data"
-import { Fragment } from "react"
-import EventSummary from "../../../components/event-detail/event-summary"
-import EventLogistics from "../../../components/event-detail/event-logistics"
-import EventContent from "../../../components/event-detail/event-content"
+import { useRouter } from "next/router";
+import { getEventById } from "../../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../../components/event-detail/event-summary";
+import EventLogistics from "../../../components/event-detail/event-logistics";
+import EventContent from "../../../components/event-detail/event-content";
+import fs from "fs/promises";
+import path from "path";
 
-const EventDetailPage = () => {
-    const router = useRouter()
-    const eventId = router.query.eventId
-    const eventInfo = getEventById(eventId)
-    console.log(eventInfo)
-    
+const EventDetailPage = (props) => {
+  // const router = useRouter();
+  // const eventId = router.query.eventId;
+  // const eventInfo = getEventById(eventId);
+  // console.log(props);
+  const { eventInfo } = props;
 
-    if (!eventInfo) {
-      return <p>No event found!</p>
-    } 
+  if (!eventInfo) {
+    return <p>No event found!</p>;
+  }
 
-    const { title, date, location, image, description } = eventInfo
+  const { title, date, location, image, description } = eventInfo;
 
   return (
     <Fragment>
-      <EventSummary title={title}/>
-      <EventLogistics date={date} address={location} image={image} imageAlt={title}/>
-      <EventContent><p>{description}</p></EventContent>
+      <EventSummary title={title} />
+      <EventLogistics
+        date={date}
+        address={location}
+        image={image}
+        imageAlt={title}
+      />
+      <EventContent>
+        <p>{description}</p>
+      </EventContent>
     </Fragment>
-  )
-}
+  );
+};
 
-export default EventDetailPage
+const getData = async () => {
+  const filePath = path.join(process.cwd(), "dummy-data.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+};
+
+export const getStaticProps = async (context) => {
+  // useRouter doesn't work here as this is run on server side before loading to client
+  const { params } = context;
+  const eventId = params.eventId;
+  const data = await getData();
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+  if (data.length === 0) {
+    return { notFound: true };
+  }
+
+  const eventInfo = data.find((ev) => ev.id === eventId);
+
+  if (!eventInfo) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      eventInfo: eventInfo,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const data = await getData();
+  const ids = data.map((ev) => ev.id);
+  const params = ids.map((id) => ({ params: { eventId: id } }));
+  return {
+    paths: params,
+    fallback: true, //allows paths not specified in getStaticPaths
+  };
+};
+
+export default EventDetailPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,41 @@
 import EventList from "../../components/events/eventList";
-import { getFeaturedEvents } from "../../dummy-data";
+import { DUMMY_EVENTS, getFeaturedEvents } from "../../dummy-data";
+import fs from "fs/promises";
+import path from "path";
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
+const HomePage = (props) => {
+  // const featuredEvents = getFeaturedEvents();
 
   return (
     <>
-      <EventList items={featuredEvents} />
+      {/* <EventList items={featuredEvents} /> */}
+      <EventList items={props.events} />
     </>
   );
+};
+
+export const getStaticProps = async (context) => {
+  const filePath = path.join(process.cwd(), "dummy-data.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+  if (data.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      events: data,
+    },
+    revalidate: 600,
+  };
 };
 
 export default HomePage;
